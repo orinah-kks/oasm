4e962acb5c4d967d81d91ee2d100c77e5123b734
Mikekkamara <mikekkamara@gmail.com>
2021-11-02 16:48:16 +0300

    Added marks option

--- resources/views/student_assignment/index.blade.php
+++ resources/views/student_assignment/index.blade.php
@@ -4,44 +4,40 @@
 
 <div class="page-wrapper">
         <div class="col-md-12">
-
-                      
-
                 <div class="card">
 
-                <div class="card-block">
-                
-                        <a href="{{route('student_assignment.submited')}}" class="btn btn-primary pull-right" style="margin-right: 1px !important;"><i class="fa fa-eye"> View Submited Assignments</i></a>
-                
-                        <a href="{{route('student_assignment.create')}}" class="btn btn-primary pull-right" style="margin-right: 1px !important;"><i class="fa fa-plus">Submit Assignment</i></a>
-                </div>
-                <div class="card-block reset-table p-t-0">
-                <div class="table-responsive">
-                <table class="table">
-                <thead>
-                <tr class="text-uppercase">
-                <th>UNIT CODE</th>
-                <th>UNIT NAME</th>
-                <th>DATE ASSIGNED</th>
-                <th>ACTION</th>
-                 </tr>
-                </thead>
-                <tbody>
-                @foreach ($datas as $item)
-                
-                        <tr>
-                        <td>{{ $item->unit_code }}</td>
-                        <td>{{ $item->unit_name }}</td>
-                        <td>Hellop</td>
-                        <td>{{$item->updated_at}}</td>
-                        <td><a href="{{route('student_assignment.download',$item->assign_id)}}"><i class="fa fa-download"></i></a></td> 
+                        <div class="card-block">
+                        
+                                <a href="{{route('student_assignment.submited')}}" class="btn btn-primary pull-right" style="margin-right: 1px !important;"><i class="fa fa-eye"> View Submited Assignments</i></a>
+                        
+                                <a href="{{route('student_assignment.create')}}" class="btn btn-primary pull-right" style="margin-right: 1px !important;"><i class="fa fa-plus">Submit Assignment</i></a>
+                        </div>
+                        <div class="card-block reset-table p-t-0">
+                        <div class="table-responsive">
+                        <table class="table">
+                        <thead>
+                        <tr class="text-uppercase">
+                        <th>UNIT CODE</th>
+                        <th>UNIT NAME</th>
+                        <th>DATE ASSIGNED</th>
+                        <th>ACTION</th>
                         </tr>
-                @endforeach
-                </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                        @foreach ($datas as $item)
+                        
+                                <tr>
+                                <td>{{ $item->unit_code }}</td>
+                                <td>{{ $item->unit_name }}</td>
+                                <td>{{$item->updated_at}}</td>
+                                <td><a href="{{route('student_assignment.download',$item->assign_id)}}"><i class="fa fa-download"></i></a></td> 
+                                </tr>
+                        @endforeach
+                        </tbody>
+                        </table>
+                        </div>
+                        </div>
                 </div>
-                </div
-                </div>
-                </div>
+        </div>
 </div>
 @endsection
