f4122530ee0b8f506de8584c13fa2df0e0c79589
Mikekkamara <mikekkamara@gmail.com>
2021-11-02 13:49:33 +0300

    initialization

--- app/User.php
+++ app/User.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
+use Spatie\Permission\Models\Role;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
@@ -40,4 +40,5 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;   
 
 Route::get('/', function () {
     return view('welcome');
